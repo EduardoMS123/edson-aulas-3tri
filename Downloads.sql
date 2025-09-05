4b0554b56f10478dca0d9a6a090bf52bd42a1de9
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2025-09-05 11:53:23 -0300

    Add files via upload

--- Downloads.sql
+++ Downloads.sql
@@ -1,9 +1,18 @@
-create table tb_pedido(
-cod_pedido int auto_increment,
-data date not null,
-valor_pedido decimal(10,2) not null,
-cod_cliente int not null,
-primary key (cod_pedido),
-constraint fk_pedido_cliente foreign key(cod_cliente)
-references Tb_CLIENTE(cod_cliente)
+create table tb_livro(
+cod_livro int auto_increment,
+valor decimal(10,2) not null,
+ano_publicacao int not null,
+categoria varchar(25) not null,
+titulo varchar(50) not null,
+isbn varchar(25) not null,
+autor varchar(25) not null,
+cod_editora int not null,
+primary key(cod_livro)
 );
+
+
+
+
+
+
+
